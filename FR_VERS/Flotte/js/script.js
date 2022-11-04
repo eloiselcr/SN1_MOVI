dee8cccc1cfc86ce4c72e6593763aa372300e07b
eloiselcr <eloise.lecronier@gmail.com>
2022-11-04 03:12:55 +0100

    Retire Compet + Motorisation, ajout des pages Flotte

--- FR_VERS/Flotte/js/script.js
+++ FR_VERS/Flotte/js/script.js
@@ -40,27 +40,6 @@
 		autoplaySpeed: 4000
 	});
 
-
-	/* ========================================================================= */
-	/*	Magnific popup
-	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
-	});
-
-
 	// counterUp
 	function counter() {
 		var oTop;
